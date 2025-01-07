b0fdb80e6b9fc503e419d4f3840e066f2a3fbf8e
Yaroslav <q.link0.p@gmail.com>
2025-01-07 15:04:37 +0200

    Ran through swift format. Simplified internal decoding flow

--- Sources/NSCollectionCoder/NSCollectionCoder.swift
+++ Sources/NSCollectionCoder/NSCollectionCoder.swift
@@ -1,7 +1,5 @@
 import Foundation
 
-
-
 public func decode<T: Decodable>(_: T.Type, fromNSCollection collection: Any) throws -> T {
     try T(from: NSCollectionDecoder(value: collection, codingPath: []))
 }
@@ -9,29 +7,43 @@ public func decode<T: Decodable>(_: T.Type, fromNSCollection collection: Any) th
 private struct NSCollectionDecoder: Decoder {
     private let value: Any
     var codingPath: [any CodingKey]
-    
+
     init(value: Any, codingPath: [any CodingKey]) {
         self.value = value
         self.codingPath = codingPath
     }
-    
-    var userInfo: [CodingUserInfoKey : Any] { [:] }
-    
-    func container<Key>(keyedBy keyType: Key.Type) throws -> KeyedDecodingContainer<Key> where Key : CodingKey {
+
+    var userInfo: [CodingUserInfoKey: Any] { [:] }
+
+    func container<Key>(keyedBy keyType: Key.Type) throws -> KeyedDecodingContainer<Key>
+    where Key: CodingKey {
         guard let dict = value as? NSDictionary else {
-            throw DecodingError.typeMismatch(NSDictionary.self, DecodingError.Context(codingPath: self.codingPath, debugDescription: "Expected NSDictionary but found \(type(of: value))."))
+            throw DecodingError.typeMismatch(
+                NSDictionary.self,
+                DecodingError.Context(
+                    codingPath: self.codingPath,
+                    debugDescription: "Expected NSDictionary but found \(type(of: value))."
+                )
+            )
         }
-        return KeyedDecodingContainer(DictionaryDecodingContainer(
-            of: dict, keyedBy: keyType, codingPath: codingPath))
+        return KeyedDecodingContainer(
+            DictionaryDecodingContainer(
+                of: dict, keyedBy: keyType, codingPath: codingPath))
     }
-    
+
     func unkeyedContainer() throws -> any UnkeyedDecodingContainer {
         guard let array = value as? NSArray else {
-            throw DecodingError.typeMismatch(NSDictionary.self, DecodingError.Context(codingPath: self.codingPath, debugDescription: "Expected NSArray but found \(type(of: value))."))
+            throw DecodingError.typeMismatch(
+                NSDictionary.self,
+                DecodingError.Context(
+                    codingPath: self.codingPath,
+                    debugDescription: "Expected NSArray but found \(type(of: value))."
+                )
+            )
         }
         return ArrayDecodingContainer(of: array, codingPath: codingPath)
     }
-    
+
     func singleValueContainer() throws -> any SingleValueDecodingContainer {
         return TargetValueContainer(value, codingPath: codingPath)
     }
@@ -55,99 +67,98 @@ private struct DictionaryDecodingContainer<Key: CodingKey>: KeyedDecodingContain
     func contains(_ key: Key) -> Bool {
         dictionary.value(forKey: key.stringValue) != nil
     }
-    
+
     func decodeNil(forKey key: Key) throws -> Bool {
-        try withElement(at: key) { $0 is NSNull }
+        try element(at: key) is NSNull
     }
-    
-    func withElement<T>(at key: Key, _ body: (Any) throws -> T) throws -> T {
+
+    func element(at key: Key) throws -> Any {
         guard let value = dictionary.value(forKey: key.stringValue) else {
-            throw DecodingError.keyNotFound(key, DecodingError.Context(
-                codingPath: self.codingPath,
-                debugDescription: "Dictionary key \(key.stringValue.debugDescription) not found in \(self.dictionary)"
-            ))
+            throw DecodingError.keyNotFound(
+                key,
+                DecodingError.Context(
+                    codingPath: self.codingPath,
+                    debugDescription:
+                        "Dictionary key \(key.stringValue.debugDescription) not found in \(self.dictionary)"
+                )
+            )
         }
-        return try body(value)
+        return value
     }
-    
+
+    func element<T>(at key: Key, ofType: T.Type) throws -> T {
+        let element = try element(at: key)
+        guard let value = element as? T else {
+            throw DecodingError.typeMismatch(
+                T.self,
+                DecodingError.Context(
+                    codingPath: self.codingPath + [key],
+                    debugDescription:
+                        "Expected value at key \(key.stringValue.debugDescription) to be \(T.self), not \(element)"
+                )
+            )
+        }
+        return value
+    }
+
     func nestedContainer<NestedKey>(
         keyedBy keyType: NestedKey.Type,
         forKey key: Key
-    ) throws -> KeyedDecodingContainer<NestedKey> where NestedKey : CodingKey {
-        try withElement(at: key) { value in
-            guard let nestedDictionary = value as? NSDictionary else {
-                throw DecodingError.typeMismatch(NSDictionary.self, DecodingError.Context(
-                    codingPath: self.codingPath + [key],
-                    debugDescription: "Expected value at key \(key.stringValue.debugDescription) to be NSDictionary, not \(value)"
-                ))
-            }
-            return KeyedDecodingContainer(DictionaryDecodingContainer<NestedKey>(
-                of: nestedDictionary, keyedBy: keyType, codingPath: codingPath + [key]))
-        }
+    ) throws -> KeyedDecodingContainer<NestedKey> where NestedKey: CodingKey {
+        return KeyedDecodingContainer(
+            DictionaryDecodingContainer<NestedKey>(
+                of: try element(at: key, ofType: NSDictionary.self),
+                keyedBy: keyType,
+                codingPath: codingPath + [key]
+            )
+        )
     }
-    
+
     func nestedUnkeyedContainer(forKey key: Key) throws -> any UnkeyedDecodingContainer {
-        try withElement(at: key) { value in
-            guard let array = value as? NSArray else {
-                throw DecodingError.typeMismatch(NSArray.self, DecodingError.Context(
-                    codingPath: self.codingPath + [key],
-                    debugDescription: "Expected value at key \(key.stringValue.debugDescription) to be NSArray, not \(value)"
-                ))
-            }
-            return ArrayDecodingContainer(of: array, codingPath: self.codingPath + [key])
-        }
+        let array = try element(at: key, ofType: NSArray.self)
+        return ArrayDecodingContainer(of: array, codingPath: self.codingPath + [key])
     }
-    
+
     func superDecoder() throws -> any Decoder {
         fatalError("I have no idea how to implement inheritance decoding")
     }
-    
+
     func superDecoder(forKey key: Key) throws -> any Decoder {
         fatalError("I have no idea how to implement inheritance decoding")
     }
-    
+
     func decode<T: Decodable>(_ type: T.Type, forKey key: Key) throws -> T {
-        try withElement(at: key) { value in
-            try T(from: NSCollectionDecoder(value: value, codingPath: codingPath + [key]))
-        }
+        let decoder = NSCollectionDecoder(
+            value: try element(at: key),
+            codingPath: codingPath + [key]
+        )
+        return try T(from: decoder)
     }
-    
-    func decodePrimitive<T>(_ type: T.Type, forKey key: Key) throws -> T {
-        try withElement(at: key) { value in
-            guard let casted = value as? T else {
-                throw DecodingError.typeMismatch(T.self, DecodingError.Context(
-                    codingPath: codingPath + [key],
-                    debugDescription: "Expected value at key \(key.stringValue.debugDescription) to be \(T.self), not \(value)"
-                ))
-            }
-            return casted
-        }
-    }
-    
+
     func decode(_ type: Bool.Type, forKey key: Key) throws -> Bool {
-        try decodePrimitive(Bool.self, forKey: key)
+        try element(at: key, ofType: Bool.self)
     }
-    
+
     func decode(_ type: String.Type, forKey key: Key) throws -> String {
-        try decodePrimitive(String.self, forKey: key)
+        try element(at: key, ofType: String.self)
     }
-    
+
     func decode(_ type: Double.Type, forKey key: Key) throws -> Double {
-        try decodePrimitive(Double.self, forKey: key)
+        try element(at: key, ofType: Double.self)
     }
-    
+
     func decode(_ type: Float.Type, forKey key: Key) throws -> Float {
-        try decodePrimitive(Float.self, forKey: key)
+        try element(at: key, ofType: Float.self)
     }
-    
+
     func decode(_ type: Int.Type, forKey key: Key) throws -> Int {
-        try decodePrimitive(Int.self, forKey: key)
+        try element(at: key, ofType: Int.self)
     }
-    
+
     func decode(_ type: UInt.Type, forKey key: Key) throws -> UInt {
-        try decodePrimitive(UInt.self, forKey: key)
+        try element(at: key, ofType: UInt.self)
     }
-    
+
     // TODO: Fixed-size integers
 }
 
@@ -175,156 +186,162 @@ private struct ArrayDecodingContainer: UnkeyedDecodingContainer {
         self.codingPath = codingPath
         self.currentIndex = 0
     }
-    
+
     var count: Int? { array.count }
 
     var isAtEnd: Bool { currentIndex >= array.count }
-    
+
+    mutating func nextElement(expectedToBe elementType: Any.Type) throws -> (Any, ArrayKey) {
+        guard currentIndex < array.count else {
+            throw DecodingError.valueNotFound(
+                elementType,
+                DecodingError.Context(
+                    codingPath: codingPath,
+                    debugDescription:
+                        "NSArray out of bounds (currentIndex: \(currentIndex), count: \(array.count))"
+                )
+            )
+        }
+        let result = array[currentIndex]
+        let key = ArrayKey(currentIndex)
+        currentIndex += 1
+        return (result, key)
+    }
+
+    mutating func nextElement<T>(ofType elementType: T.Type) throws -> (T, [any CodingKey]) {
+        let (element, key) = try nextElement(expectedToBe: elementType.self)
+        let codingPath = self.codingPath + [key]
+
+        guard let result = element as? T else {
+            throw DecodingError.typeMismatch(
+                elementType,
+                DecodingError.Context(
+                    codingPath: codingPath,
+                    debugDescription:
+                        "Expected element at position \(key.rawValue) to be a \(elementType), not \(element) of type \(type(of: element))"
+                )
+            )
+        }
+        return (result, codingPath)
+    }
+
     mutating func decodeNil() throws -> Bool {
-        try withElement { $0 is NSNull }
+        let (next, _) = try nextElement(expectedToBe: NSNull.self)
+        return next is NSNull
     }
-    
-    mutating func nestedContainer<NestedKey>(keyedBy keyType: NestedKey.Type) throws -> KeyedDecodingContainer<NestedKey> where NestedKey : CodingKey {
-        let key = ArrayKey(currentIndex)
-        let codingPath = self.codingPath + [key]
-        return try withElement { element in
-            guard let nestedDictionary = element as? NSDictionary else {
-                throw DecodingError.typeMismatch(NSDictionary.self, DecodingError.Context(
-                    codingPath: codingPath,
-                    debugDescription: "Expected element at position \(key.rawValue) to be NSDictionary, not \(element)"
-                ))
-            }
-            return KeyedDecodingContainer(DictionaryDecodingContainer<NestedKey>(
-                of: nestedDictionary, keyedBy: keyType, codingPath: codingPath))
-        }
+
+    mutating func nestedContainer<NestedKey>(keyedBy keyType: NestedKey.Type) throws
+        -> KeyedDecodingContainer<NestedKey> where NestedKey: CodingKey
+    {
+        let (nestedDictionary, codingPath) = try nextElement(ofType: NSDictionary.self)
+        return KeyedDecodingContainer(
+            DictionaryDecodingContainer<NestedKey>(
+                of: nestedDictionary,
+                keyedBy: keyType,
+                codingPath: codingPath
+            )
+        )
     }
-    
+
     mutating func nestedUnkeyedContainer() throws -> any UnkeyedDecodingContainer {
-        let key = ArrayKey(currentIndex)
-        let codingPath = self.codingPath + [key]
-        return try withElement { element in
-            guard let nestedArray = element as? NSArray else {
-                throw DecodingError.typeMismatch(NSArray.self, DecodingError.Context(
-                    codingPath: codingPath,
-                    debugDescription: "Expected element at position \(key.rawValue) to be an NSArray, not \(element)"
-                ))
-            }
-            return ArrayDecodingContainer(of: nestedArray, codingPath: codingPath)
-        }
+        let (nestedArray, codingPath) = try nextElement(ofType: NSArray.self)
+        return ArrayDecodingContainer(of: nestedArray, codingPath: codingPath)
     }
-    
+
     mutating func superDecoder() throws -> any Decoder {
         fatalError("I have no idea how to implement inheritance decoder")
     }
-    
-    mutating func withElement<T>(_ block: (Any) throws -> T) throws -> T {
-        guard currentIndex < array.count else {
-            throw DecodingError.valueNotFound(Any?.self, DecodingError.Context(
-                codingPath: codingPath,
-                debugDescription: "NSArray out of bounds (currentIndex: \(currentIndex), count: \(array.count))"
-            ))
-        }
-        let result = try block(array[currentIndex])
-        currentIndex += 1
-        return result
-    }
-    
-    mutating func decode<T>(_ type: T.Type) throws -> T where T : Decodable {
-        let key = ArrayKey(currentIndex)
-        let codingPath = self.codingPath + [key]
-        return try withElement { element in
-            try T(from: NSCollectionDecoder(value: element, codingPath: codingPath))
-        }
+
+    mutating func decode<T>(_ type: T.Type) throws -> T where T: Decodable {
+        let (element, key) = try nextElement(expectedToBe: type)
+        return try T(from: NSCollectionDecoder(value: element, codingPath: codingPath + [key]))
     }
 
     mutating func decodePrimitive<T>(_ type: T.Type) throws -> T {
-        let key = ArrayKey(currentIndex)
-        let codingPath = self.codingPath + [key]
-        return try withElement { element in
-            guard let casted = element as? T else {
-                throw DecodingError.typeMismatch(T.self, DecodingError.Context(
-                    codingPath: codingPath,
-                    debugDescription: "Expected element at position \(key.rawValue) to be \(T.self), not \(element)"
-                ))
-            }
-            return casted
-        }
+        let (element, _) = try nextElement(ofType: type)
+        return element
     }
-    
+
     mutating func decode(_ type: Bool.Type) throws -> Bool {
         try decodePrimitive(Bool.self)
     }
-    
+
     mutating func decode(_ type: String.Type) throws -> String {
         try decodePrimitive(String.self)
     }
-    
+
     mutating func decode(_ type: Double.Type) throws -> Double {
         try decodePrimitive(Double.self)
     }
-    
+
     mutating func decode(_ type: Float.Type) throws -> Float {
         try decodePrimitive(Float.self)
     }
-    
+
     mutating func decode(_ type: Int.Type) throws -> Int {
         try decodePrimitive(Int.self)
     }
-    
+
     mutating func decode(_ type: UInt.Type) throws -> UInt {
         try decodePrimitive(UInt.self)
     }
-    
+
     // TODO: Fixed-sized types
 }
 
 private struct TargetValueContainer: SingleValueDecodingContainer {
     private let value: Any
     var codingPath: [any CodingKey]
-    
+
     init(_ value: Any, codingPath: [any CodingKey]) {
         self.value = value
         self.codingPath = codingPath
     }
-    
+
     func decodeNil() -> Bool {
         value is NSNull
     }
-    
-    func decode<T>(_ type: T.Type) throws -> T where T : Decodable {
+
+    func decode<T>(_ type: T.Type) throws -> T where T: Decodable {
         try T(from: NSCollectionDecoder(value: self, codingPath: codingPath))
     }
 
     func decodePrimitive<T>(_ type: T.Type) throws -> T {
         guard let self = value as? T else {
-            throw DecodingError.typeMismatch(T.self, DecodingError.Context(codingPath: codingPath, debugDescription: "Value \(value) is not \(T.self)"))
+            throw DecodingError.typeMismatch(
+                T.self,
+                DecodingError.Context(
+                    codingPath: codingPath,
+                    debugDescription: "Value \(value) is not \(T.self)"
+                )
+            )
         }
         return self
     }
-    
+
     func decode(_ type: Bool.Type) throws -> Bool {
         try decodePrimitive(Bool.self)
     }
-    
+
     func decode(_ type: String.Type) throws -> String {
         try decodePrimitive(String.self)
     }
-    
+
     func decode(_ type: Double.Type) throws -> Double {
         try decodePrimitive(Double.self)
     }
-    
+
     func decode(_ type: Float.Type) throws -> Float {
         try decodePrimitive(Float.self)
     }
-    
+
     func decode(_ type: Int.Type) throws -> Int {
         try decodePrimitive(Int.self)
     }
-    
+
     func decode(_ type: UInt.Type) throws -> UInt {
         try decodePrimitive(UInt.self)
     }
-    
+
     // TODO: Fixed-sized types
 }
--- Tests/NSCollectionCoderTests/NSCollectionCoderTests.swift
+++ Tests/NSCollectionCoderTests/NSCollectionCoderTests.swift
@@ -1,43 +1,48 @@
-import Testing
-@testable import NSCollectionCoder
 import Foundation
+import Testing
+
+@testable import NSCollectionCoder
 
 @Test func flatSample() async throws {
-    let input = [
-        "foo": "bar" as NSString,
-        "baz": 42 as NSNumber
-    ] as NSDictionary
-    
+    let input =
+        [
+            "foo": "bar" as NSString,
+            "baz": 42 as NSNumber,
+        ] as NSDictionary
+
     struct Target: Decodable, Equatable {
         var foo: String
         var baz: Int
     }
-    
+
     let output = try decode(Target.self, fromNSCollection: input)
     #expect(output == Target(foo: "bar", baz: 42))
 }
 
 @Test func nestedArray() async throws {
-    let input = [
-        "foo": [
-            ["bar": 420],
-            ["bar": 69]
-        ]
-    ] as NSDictionary
-    
+    let input =
+        [
+            "foo": [
+                ["bar": 420],
+                ["bar": 69],
+            ]
+        ] as NSDictionary
+
     struct Inner: Decodable, Equatable {
         var bar: Int
     }
-    
+
     struct Target: Decodable, Equatable {
         var foo: [Inner]
     }
-    
+
     let output = try decode(Target.self, fromNSCollection: input)
-    #expect(output == Target(foo: [
-        Inner(bar: 420),
-        Inner(bar: 69),
-    ]))
+    #expect(
+        output
+            == Target(foo: [
+                Inner(bar: 420),
+                Inner(bar: 69),
+            ]))
 }
 
 @Test func nested() async throws {
@@ -47,37 +52,38 @@ import Foundation
         ],
         "thingy2": [
             "foo": ["bar": 2]
-        ]
+        ],
     ]
-    
+
     struct Inner: Decodable, Equatable {
         var bar: Int
     }
-    
+
     struct Target: Decodable, Equatable {
         var foo: Inner
     }
-    
+
     let output = try decode([String: Target].self, fromNSCollection: input)
-    
-    #expect(output == [
-        "thingy": Target(foo: Inner(bar: 1)),
-        "thingy2": Target(foo: Inner(bar: 2))
-    ])
+
+    #expect(
+        output == [
+            "thingy": Target(foo: Inner(bar: 1)),
+            "thingy2": Target(foo: Inner(bar: 2)),
+        ])
 }
 
 @Test func optionalField() async throws {
     let input = [
         "required": "foo",
-        "optional": "bar"
+        "optional": "bar",
     ]
-    
+
     struct Target: Decodable, Equatable {
         var required: String
         var optional: String?
         var missing: String?
     }
-    
+
     let output = try decode(Target.self, fromNSCollection: input)
     #expect(output == Target(required: "foo", optional: "bar", missing: nil))
 }
@@ -85,15 +91,15 @@ import Foundation
 @Test func failOnMissingKey() async throws {
     let input = [
         "required": "foo",
-        "optional": "bar"
+        "optional": "bar",
     ]
-    
+
     struct Target: Decodable, Equatable {
         var required: String
         var optional: String
         var missing: String
     }
-    
+
     #expect(throws: DecodingError.self) {
         try decode(Target.self, fromNSCollection: input)
     }
